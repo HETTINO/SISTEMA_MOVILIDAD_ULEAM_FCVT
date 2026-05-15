fdef08a55e08e58e396e00bee141c92583ec7e91
Cristina <cristynohecedeno@gmail.com>
2026-05-15 01:59:54 -0500

    Aporte Cristina: Estructura inicial y Entidad Solicitud

--- internal/solicitud.go
+++ internal/solicitud.go
@@ -1,9 +1,12 @@
 package internal
 
 // Solicitud: Entidad diseñada por Cristina para el módulo de transporte.
+
 type Solicitud struct {
-	ID               uint   `json:"id"`
-	CantidadPersonas int    `json:"cantidad_personas"`
-	Destino          string `json:"destino"`
-	Estado           string `json:"estado"`
+	ID             uint   `json:"id_solicitud"`
+	CedulaUsuario  string `json:"fk_cedula_usuario"`
+	IDCarrito      int    `json:"fk_id_carrito"`
+	CantPersonas   int    `json:"cant_personas"`
+	PuntoDestino   string `json:"punto_destino"`
+	Estado         string `json:"estado"`
 }
